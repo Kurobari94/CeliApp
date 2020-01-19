e6043d5d1d3e8072c313650c15c7acc9a13dc411
Kurobari94 <rodrigo.94@outlook.com>
2020-01-19 10:11:34 -0300

    Direccionamiento de los botones a sus respectivas actrividades

--- app/src/main/java/com/example/celiapp/MainActivity.java
+++ app/src/main/java/com/example/celiapp/MainActivity.java
@@ -15,6 +15,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         View v = findViewById(R.id.btn_recetas);
         v.setOnClickListener(this);
+
+        View w = findViewById(R.id.btn_ubicacion);
+        w.setOnClickListener(this);
+
+        View x = findViewById(R.id.btn_adicional);
+        x.setOnClickListener(this);
+
+        View y = findViewById(R.id.btn_productos);
+        y.setOnClickListener(this);
     }
 
     @Override
@@ -23,6 +32,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (arg0.getId() == R.id.btn_recetas){
             //definir el intento para la actividad(lo que se quiere hacer)
             Intent intent = new Intent(this,RecetasActivity.class);
+
+            //comenzar la actividad
+            this.startActivity(intent);
+        }
+
+        else if (arg0.getId() == R.id.btn_ubicacion){
+            //definir el intento para la actividad(lo que se quiere hacer)
+            Intent intent = new Intent(this,UbicacionActivity.class);
+
+            //comenzar la actividad
+            this.startActivity(intent);
+        }
+
+        else if (arg0.getId() == R.id.btn_adicional){
+            //definir el intento para la actividad(lo que se quiere hacer)
+            Intent intent = new Intent(this,AdicionalActivity.class);
+
+            //comenzar la actividad
+            this.startActivity(intent);
+        }
+
+        else if (arg0.getId() == R.id.btn_productos){
+            //definir el intento para la actividad(lo que se quiere hacer)
+            Intent intent = new Intent(this,ProductosActivity.class);
 
             //comenzar la actividad
             this.startActivity(intent);
