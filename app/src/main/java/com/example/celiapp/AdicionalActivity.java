5047de8545e24677c387621d973ca49b907b2bfd
Kurobari94 <rodrigo.94@outlook.com>
2020-01-18 22:59:01 -0300

    Agregado las actividades Adicional, Productos y Ubicacion.

---
+++ app/src/main/java/com/example/celiapp/AdicionalActivity.java
@@ -0,0 +1,14 @@
+package com.example.celiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdicionalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicional);
+    }
+}
--- app/src/main/java/com/example/celiapp/MainActivity.java
+++ app/src/main/java/com/example/celiapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.celiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View v = findViewById(R.id.btn_recetas);
+        v.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View arg0) {
+
+        if (arg0.getId() == R.id.btn_recetas){
+            //definir el intento para la actividad(lo que se quiere hacer)
+            Intent intent = new Intent(this,RecetasActivity.class);
+
+            //comenzar la actividad
+            this.startActivity(intent);
+        }
     }
 }
