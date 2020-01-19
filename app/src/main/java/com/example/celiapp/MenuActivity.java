519e65d9dd61578674404750fbe40238a5a7833d
Kurobari94 <rodrigo.94@outlook.com>
2020-01-19 11:57:42 -0300

    Renombrado la Main Activity a Menu Activity

--- app/src/main/java/com/example/celiapp/MenuActivity.java
+++ app/src/main/java/com/example/celiapp/MenuActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
